e0f59e061d551c98a8616a8e4904b59f58ee6aa1
ngicks <yknt.bsl@gmail.com>
2022-09-05 17:01:33 +0000

    test(common): simplify and parallelize tests

--- pkg/common/timer_test.go
+++ pkg/common/timer_test.go
@@ -11,6 +11,8 @@ import (
 )
 
 func TestTimerRealStop(t *testing.T) {
+	t.Parallel()
+
 	timer := common.NewTimerReal()
 
 	assertNotExpired := func() {
@@ -35,6 +37,7 @@ func TestTimerRealStop(t *testing.T) {
 		t.Fatal()
 	}
 
+	// Making sure that Reset(0) and time.Sleep combo really expires the timer.
 	timer.Reset(0)
 	time.Sleep(time.Microsecond)
 	select {
@@ -50,26 +53,14 @@ func TestTimerRealStop(t *testing.T) {
 }
 
 func TestTimerRealReset(t *testing.T) {
-	type testCase struct {
-		d time.Duration
-	}
+	t.Parallel()
 
-	cases := []testCase{
-		{
-			d: time.Millisecond,
-		},
-		{
-			d: time.Microsecond,
-		},
-		{
-			d: time.Second,
-		},
-		{
-			d: time.Second + time.Millisecond*50,
-		},
-		{
-			d: 2 * time.Second,
-		},
+	cases := []time.Duration{
+		time.Millisecond,
+		time.Microsecond,
+		time.Second,
+		time.Second + time.Millisecond*50,
+		2 * time.Second,
 	}
 
 	for idx, testCase := range cases {
@@ -80,36 +71,24 @@ func TestTimerRealReset(t *testing.T) {
 			timer := common.NewTimerReal()
 
 			now := time.Now()
-			timer.Reset(tt.d)
+			timer.Reset(tt)
 			<-timer.Channel()
 			then := time.Now()
-			require.GreaterOrEqual(t, int64(then.Sub(now)), int64(tt.d))
+			require.GreaterOrEqual(t, int64(then.Sub(now)), int64(tt))
 		})
 	}
 }
 
 func TestTimerRealResetTo(t *testing.T) {
-	type testCase struct {
-		to time.Time
-	}
+	t.Parallel()
 
 	now := time.Now()
-	cases := []testCase{
-		{
-			to: now.Add(time.Millisecond),
-		},
-		{
-			to: now.Add(time.Microsecond),
-		},
-		{
-			to: now.Add(time.Second),
-		},
-		{
-			to: now.Add(time.Second + time.Millisecond*50),
-		},
-		{
-			to: now.Add(2 * time.Second),
-		},
+	cases := []time.Time{
+		now.Add(time.Millisecond),
+		now.Add(time.Microsecond),
+		now.Add(time.Second),
+		now.Add(time.Second + time.Millisecond*50),
+		now.Add(2 * time.Second),
 	}
 
 	for idx, testCase := range cases {
@@ -119,10 +98,10 @@ func TestTimerRealResetTo(t *testing.T) {
 
 			timer := common.NewTimerReal()
 
-			timer.ResetTo(tt.to)
+			timer.ResetTo(tt)
 			<-timer.Channel()
 			then := time.Now()
-			require.InDelta(t, then.UnixNano(), tt.to.UnixNano(), float64(10*time.Millisecond))
+			require.InDelta(t, then.UnixNano(), tt.UnixNano(), float64(10*time.Millisecond))
 		})
 	}
 }
