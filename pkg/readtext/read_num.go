fe441b28f67cad3735576766f69f1980f34001af
ngicks <yknt.bsl@gmail.com>
2022-09-26 13:24:27 +0000

    feat(readtext): add text helper

---
+++ pkg/readtext/read_num.go
@@ -0,0 +1,106 @@
+// readtext is collection of trivial text-reader functions.
+//
+// Some of code is copied from Go standard packages. So let's keep it creditted.
+//
+// Copyright (c) 2009 The Go Authors. All rights reserved.
+//
+// Full note can be also found in ./GO_LICENSE.
+//
+// Parts modified or written by me are governed by license that can be found in LICENSE.
+package readtext
+
+import "unicode/utf8"
+
+// EqualAsciiCaseInsensitiveStrict reports s1 and s2 is same string in case-insensitive way,
+// assuming s1 and s2 only contain ascii code.
+func EqualAsciiCaseInsensitive(s1, s2 string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	return EqualAsciiCaseInsensitiveStrict(s1, s2)
+}
+
+// EqualAsciiCaseInsensitiveStrict reports s1 and s2 is same string in case-insensitive way,
+// assuming s1 and s2 only contain ascii code and are same len.
+func EqualAsciiCaseInsensitiveStrict(s1, s2 string) bool {
+	for i := 0; i < len(s1); i++ {
+		c1 := s1[i]
+		c2 := s2[i]
+		if c1 != c2 {
+			// Switch to lower-case; 'a'-'A' is known to be a single bit. (0b100000)
+			c1 |= 'a' - 'A'
+			c2 |= 'a' - 'A'
+			if c1 != c2 || c1 < 'a' || c1 > 'z' {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// ReadRuneN cuts n utf-8 code points from val and returns as runes, returns rest as remaining.
+func ReadRuneN(val string, n int) (runes string, remaining string) {
+	var offset int
+	for i := 0; i < n; i++ {
+		_, size := utf8.DecodeRune([]byte(val[offset:]))
+		if size == 0 {
+			break
+		}
+		offset += size
+	}
+	return val[0:offset], val[offset:]
+}
+
+// IsDigit reports i-th is in range and i-th byte of val is digit of ascii code.
+func IsDigit(val string, i int) bool {
+	if len(val) <= i {
+		return false
+	}
+	c := val[i]
+	return '0' <= c && c <= '9'
+}
+
+// ReadNum2 cuts up to 2 digits from input val, returns parsed num as int and remaining string.
+// It reports successful operation by returning true found.
+// if shouldBePadded is true, 1st and 2nd byte of val must be digit (namely '0' <= chara <= '9').
+func ReadNum2(val string, shouldBePadded bool) (num int, remaining string, found bool) {
+	if !IsDigit(val, 0) {
+		return 0, val, false
+	}
+	if !IsDigit(val, 1) {
+		if shouldBePadded {
+			return 0, val, false
+		}
+		return int(val[0] - '0'), val[1:], true
+	}
+	return int(val[0]-'0')*10 + int(val[1]-'0'), val[2:], true
+}
+
+// ReadNumN reads up to n digits from head of val, returns string parsed as number.
+// remaining is rest of string.
+// It reports successful operation by returning true found.
+// if shouldBePadded is true, first n bytes must be digits (namely '0' <= chara <= '9').
+func ReadNumN(val string, shouldBePadded bool, n int) (num int, remaining string, found bool) {
+	var i int
+	for ; i < n && IsDigit(val, i); i++ {
+		num = num*10 + int(val[i]-'0')
+	}
+	if i == 0 || (shouldBePadded && i != n) {
+		return 0, val, false
+	}
+	return num, val[i:], true
+}
+
+func ReadNumSpN(val string, n int) (num int, remaining string, found bool) {
+	var i int
+	for ; i < n && (val[i] == ' ' || IsDigit(val, i)); i++ {
+		if val[i] != ' ' {
+			found = true
+			num = num*10 + int(val[i]-'0')
+		}
+	}
+	if i == 0 || i != n || !found {
+		return 0, val, false
+	}
+	return num, val[i:], true
+}
---
+++ pkg/readtext/read_num_test.go
@@ -0,0 +1,136 @@
+package readtext_test
+
+import (
+	"testing"
+
+	"github.com/ngicks/gommon/pkg/readtext"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEqualAsciiCaseInsensitive(t *testing.T) {
+	func() {
+		defer func() {
+			assert.NotNil(t, recover())
+		}()
+		assert.Equal(t, false, readtext.EqualAsciiCaseInsensitive("aaaa", "aaa"))
+		_ = readtext.EqualAsciiCaseInsensitiveStrict("aaaa", "aaa")
+	}()
+
+	for _, v := range [][2]string{{"aaa", "aaa"}, {"ABC", "aBc"}, {"あいうえ", "あいうえ"}} {
+		assert.Equal(t, true, readtext.EqualAsciiCaseInsensitive(v[0], v[1]))
+		assert.Equal(t, true, readtext.EqualAsciiCaseInsensitiveStrict(v[0], v[1]))
+	}
+	for _, v := range [][2]string{{"aaa", "aaaa"}, {"ABC", "aBca"}} {
+		assert.Equal(t, false, readtext.EqualAsciiCaseInsensitive(v[0], v[1]))
+		// false report
+		assert.Equal(t, true, readtext.EqualAsciiCaseInsensitiveStrict(v[0], v[1]))
+	}
+
+	for _, v := range [][2]string{{"aab", "aaa"}, {"abc", "acv"}, {"aBc", "abv"}} {
+		assert.Equal(t, false, readtext.EqualAsciiCaseInsensitive(v[0], v[1]))
+		assert.Equal(t, false, readtext.EqualAsciiCaseInsensitiveStrict(v[0], v[1]))
+	}
+}
+
+func TestReadRuneN(t *testing.T) {
+	input := `fooばあbaz😂漢字`
+
+	var out, remaining string
+	out, remaining = readtext.ReadRuneN(input, 1)
+	assert.Equal(t, "f", out)
+	assert.Equal(t, "ooばあbaz😂漢字", remaining)
+
+	out, remaining = readtext.ReadRuneN(input, 4)
+	assert.Equal(t, "fooば", out)
+	assert.Equal(t, "あbaz😂漢字", remaining)
+
+	out, remaining = readtext.ReadRuneN(input, 9)
+	assert.Equal(t, "fooばあbaz😂", out)
+	assert.Equal(t, "漢字", remaining)
+
+	out, remaining = readtext.ReadRuneN(input, 11)
+	assert.Equal(t, "fooばあbaz😂漢字", out)
+	assert.Equal(t, "", remaining)
+
+	out, remaining = readtext.ReadRuneN(input, 100)
+	assert.Equal(t, "fooばあbaz😂漢字", out)
+	assert.Equal(t, "", remaining)
+}
+
+type readNumNTestCase struct {
+	input             string
+	n                 int
+	shouldBePadded    bool
+	expectedFound     bool
+	expectedNum       int
+	expectedRemaining string
+}
+
+func TestReadNum2(t *testing.T) {
+	cases := []readNumNTestCase{
+		{
+			input:             "123nnn",
+			shouldBePadded:    true,
+			expectedFound:     true,
+			expectedNum:       12,
+			expectedRemaining: "3nnn",
+		},
+		{
+			input:             "1nnn",
+			shouldBePadded:    false,
+			expectedFound:     true,
+			expectedNum:       1,
+			expectedRemaining: "nnn",
+		},
+		{
+			input:             "1nnn",
+			shouldBePadded:    true,
+			expectedFound:     false,
+			expectedNum:       0,
+			expectedRemaining: "1nnn",
+		},
+	}
+
+	for _, testCase := range cases {
+		num, remaining, found := readtext.ReadNum2(testCase.input, testCase.shouldBePadded)
+		assert.Equal(t, testCase.expectedFound, found, "%+v", testCase)
+		assert.Equal(t, testCase.expectedNum, num, "%+v", testCase)
+		assert.Equal(t, testCase.expectedRemaining, remaining, "%+v", testCase)
+	}
+}
+
+func TestReadNumN(t *testing.T) {
+	cases := []readNumNTestCase{
+		{input: "123nnn", n: 2, shouldBePadded: true, expectedNum: 12, expectedRemaining: "3nnn"},
+		{input: "123nnn", n: 3, shouldBePadded: true, expectedNum: 123, expectedRemaining: "nnn"},
+		{input: "123nnn", n: 4, shouldBePadded: false, expectedNum: 123, expectedRemaining: "nnn"},
+		{input: "12345678nnn", n: 5, shouldBePadded: true, expectedNum: 12345, expectedRemaining: "678nnn"},
+	}
+
+	for _, testCase := range cases {
+		num, remaining, found := readtext.ReadNumN(testCase.input, testCase.shouldBePadded, testCase.n)
+		assert.Equal(t, found, true)
+		assert.Equal(t, testCase.expectedNum, num)
+		assert.Equal(t, testCase.expectedRemaining, remaining)
+	}
+}
+
+func TestReadNumSpN(t *testing.T) {
+	cases := []readNumNTestCase{
+		{input: "123nnn", n: 2, expectedNum: 12, expectedFound: true, expectedRemaining: "3nnn"},
+		{input: "123nnn", n: 3, expectedNum: 123, expectedFound: true, expectedRemaining: "nnn"},
+		{input: "123nnn", n: 4, expectedNum: 0, expectedFound: false, expectedRemaining: "123nnn"},
+		{input: "12345678nnn", n: 5, expectedNum: 12345, expectedFound: true, expectedRemaining: "678nnn"},
+		{input: " 1234nnn", n: 4, expectedNum: 123, expectedFound: true, expectedRemaining: "4nnn"},
+		{input: "  234nnn", n: 4, expectedNum: 23, expectedFound: true, expectedRemaining: "4nnn"},
+		{input: "   34nnn", n: 4, expectedNum: 3, expectedFound: true, expectedRemaining: "4nnn"},
+		{input: "    4nnn", n: 4, expectedNum: 0, expectedFound: false, expectedRemaining: "    4nnn"},
+	}
+
+	for _, testCase := range cases {
+		num, remaining, found := readtext.ReadNumSpN(testCase.input, testCase.n)
+		assert.Equal(t, testCase.expectedFound, found, "%+v", testCase)
+		assert.Equal(t, testCase.expectedNum, num)
+		assert.Equal(t, testCase.expectedRemaining, remaining)
+	}
+}
